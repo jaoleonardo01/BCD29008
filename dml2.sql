d21f059f8382dffcf95f676ff4e9ad669e4619ca
jaoleonardo01 <jao@oldschool.not.br>
2016-11-06 21:49:24 -0200

    Rename dml.sql to dml2.sql

---
+++ dml2.sql
@@ -0,0 +1,14 @@
+use bcd;
+
+desc semestre;
+
+insert into semestre(ano, metade)
+    	values
+     	(
+    	2016, 1
+    	),
+        (
+        2016, 2
+        );
+        
+select * from semestre;
